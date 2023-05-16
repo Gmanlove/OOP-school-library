ce65c73baf2878725cd846d0217ffbe5067be17c
Gmanlove <emmagmanc@gmail.com>
2023-05-16 13:38:26 +0100

    Add association

--- person.rb
+++ person.rb
@@ -29,7 +29,7 @@ class Person < Nameable
   end
 
   private
-  
+
   def of_age?
     @age >= 18
   end
--- rentable.rb
+++ rentable.rb
@@ -1,20 +1,20 @@
 class Rental
-    attr_accessor :date, :book, :person
-    @@rentals = []
-  
-    def initialize(date, book, person)
-      @date = date
-      @book = book
-      @person = person
-  
-      person.rentals << self
-      book.rentals << self
-  
-      @@rentals << self
-    end
-  
-    def self.all
-      @@rentals
-    end
+  attr_accessor :date, :book, :person
+
+  @rentals = []
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+
+    person.rentals << self
+    book.rentals << self
+
+    self.class.all << self
   end
-  
+
+  def self.all
+    @rentals
+  end
+end
