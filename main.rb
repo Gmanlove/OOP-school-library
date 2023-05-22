4a835cb894c132733695a07b179bcc5015658d17
Gmanlove <emmagmanc@gmail.com>
2023-05-22 16:19:36 +0100

    Refactore code

--- main.rb
+++ main.rb
@@ -1,37 +1,45 @@
 require_relative 'app'
 
-def display_menu_options
-  puts 'Please choose an option by entering a number:'
-  puts '1- List all books'
-  puts '2- List all people'
-  puts '3- Create a person'
-  puts '4- Create a book'
-  puts '5- Create a rental'
-  puts '6- List all rentals for a given person id'
-  puts '7- Exit'
+class Menu
+  def self.display_options
+    puts 'Please choose an option by entering a number:'
+    puts '1- List all books'
+    puts '2- List all people'
+    puts '3- Create a person'
+    puts '4- Create a book'
+    puts '5- Create a rental'
+    puts '6- List all rentals for a given person id'
+    puts '7- Exit'
+  end
 end
 
-def run_app(app)
-  menu_options = {
-    '1' => :list_books,
-    '2' => :list_people,
-    '3' => :add_person,
-    '4' => :add_book,
-    '5' => :add_rental,
-    '6' => :list_rentals,
-    '7' => :exit
-  }
+class AppRunner
+  def initialize(app)
+    @app = app
+  end
 
-  loop do
-    display_menu_options
-    input = gets.chomp
+  def run
+    menu_options = {
+      '1' => :list_books,
+      '2' => :list_people,
+      '3' => :add_person,
+      '4' => :add_book,
+      '5' => :add_rental,
+      '6' => :list_rentals,
+      '7' => :exit
+    }
 
-    if menu_options.key?(input)
-      action = menu_options[input]
-      app.send(action)
-      break if action == :exit
-    else
-      puts 'Invalid input. Please try again.'
+    loop do
+      Menu.display_options
+      input = gets.chomp
+
+      if menu_options.key?(input)
+        action = menu_options[input]
+        @app.send(action)
+        break if action == :exit
+      else
+        puts 'Invalid input. Please try again.'
+      end
     end
   end
 end
@@ -39,7 +47,7 @@ end
 def main
   app = App.new
   puts 'Welcome to the School Library App!'
-  run_app(app)
+  AppRunner.new(app).run
 end
 
 main
